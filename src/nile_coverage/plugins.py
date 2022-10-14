d3c1fd752fd3c9b0bd9410dc9374781f2e889cb5
Eric Nordelo <eric.nordelo39@gmail.com>
2022-10-14 17:21:30 +0200

    fix: add xml to plugin

--- src/nile_coverage/plugins.py
+++ src/nile_coverage/plugins.py
@@ -5,13 +5,14 @@ from nile_coverage.vendor import cairo_coverage
 class CoveragePlugin:
     def __init__(self, contracts_folder, xml=False):
         self.contracts_folder = contracts_folder
+        self.xml = xml
 
     def pytest_sessionfinish(self):
         """
         Called after whole test run finished, right before
         returning the exit status to the system.
         """
-        if xml:
+        if self.xml:
             cairo_coverage.report_runs_xml(self.contracts_folder)
         else:
             cairo_coverage.report_runs(self.contracts_folder)
